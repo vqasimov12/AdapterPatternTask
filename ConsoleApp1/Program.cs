a4910d69435a6d54d3c28b7ee0507097beb630c0
vqasimov12 <q.vaqif1211@gmail.com>
2024-05-28 20:11:41 +0400

    Adapter Pattern

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+List<ISystem>systems = new List<ISystem>() {new NewSystem(),new Adapter() ,new Adapter(),new NewSystem()};
+foreach (var i in systems)
+    i.Process();
+
+
+public interface ISystem
+{
+    void Process();
+}
+public class OldSystem
+{
+    public void Execute()
+    {
+        Console.WriteLine("Old system executing...");
+    }
+}
+
+public class NewSystem : ISystem
+{
+    public void Process()
+    {
+        Console.WriteLine("New System is processing..");
+    }
+}
+
+public class Adapter :OldSystem, ISystem
+{
+    public void Process()
+    {
+        Execute();
+    }
+}
